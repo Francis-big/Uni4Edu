673a63c9b230decbe6a94ab40fcfe6c7aebd3b18
Francis_Big <francistassiga17@gmail.com>
2026-06-10 19:59:36 +0200

    Remove Gaming folder

--- Gaming/script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById("menu-btn");
-
-const mobileBtn = document.getElementById("mobile-menu");
-
-menuBtn.addEventListener("click", () => {
-    mobileBtn.classList.toggle("hidden");
-});
